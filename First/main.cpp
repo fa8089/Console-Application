d97a9a3cbf2fed1eaadf4d31b9f0458c409f6e2d
p4vlo <p4vlo@otw>
2024-05-19 23:05:02 -0500

    Initial commit.

---
+++ First/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Fictional person information
+    std::string firstName = "Faisal";
+    std::string lastName = "Afsal";
+    std::string streetAddress = "123 Main St.";
+    std::string city = "Atlanta";
+    std::string zipCode = "12345";
+
+    // Print the person's information
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
